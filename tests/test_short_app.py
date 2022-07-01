0c02c408a146c6b0c99cfb76e0cd35d7ade43185
Prabal Pathak <prabal01pathak@gmail.com>
2022-07-01 23:57:42 +0530

    add root test

--- tests/test_short_app.py
+++ tests/test_short_app.py
@@ -2,9 +2,21 @@
 Discription: Test short_app routes
 Author: Prabal Pathak
 """
+from fastapi.testclient import TestClient
+
 from short_app import __version__
+from short_app.app import app
+
+client = TestClient(app)
 
 
 def test_version():
     """test the application version"""
     assert __version__ == "0.1.0"
+
+
+def test_root():
+    """Test root route"""
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World!"}
